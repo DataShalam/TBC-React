7a42e7f09d1ee8c52b5de623ee27aa271e946ef9
DataShalam <d.shalamberidze04@gmail.com>
2024-10-03 21:37:40 +0400

    Added dynamic routing on blog page, on img elements

--- src/MainPage/Components/Post/Post.jsx
+++ src/MainPage/Components/Post/Post.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Post.css";
+import { Link } from "react-router-dom";
 
 export default function Post({
   name,
@@ -13,7 +14,9 @@ export default function Post({
   return (
     <div className="post" style={style}>
       <div className={`post-container ${isReversed ? "reversed" : ""}`}>
-        <img src={src} alt={`${name} ${surName}`} className="post-image" />
+        <Link to={`/users/${name + surName}`}>
+          <img src={src} alt={`${name} ${surName}`} className="post-image" />
+        </Link>
         <div className="post-content">
           <div>
             <h2>
