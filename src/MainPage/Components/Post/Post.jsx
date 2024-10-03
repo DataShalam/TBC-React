0999aae9e6fdd6fed8fa0c8d9448f42aca8a42a2
DataShalam <d.shalamberidze04@gmail.com>
2024-10-03 16:25:11 +0400

    Completed Main Content
    - added blog elements on Main Page
    - with uniqie name
    - with unique email
    - with unique image

--- src/MainPage/Components/Post/Post.jsx
+++ src/MainPage/Components/Post/Post.jsx
@@ -1,24 +1,29 @@
 import React from "react";
-import Fighters from "./../../../PageData/Data.js";
-
 import "./Post.css";
 
-//
-
-export default function Post({ name, surName, email, text, src }) {
-  // const fighter = Fighters[1];
+export default function Post({
+  name,
+  surName,
+  email,
+  text,
+  src,
+  style,
+  isReversed,
+}) {
   return (
-    <>
-      <div className="post-container">
+    <div className="post" style={style}>
+      <div className={`post-container ${isReversed ? "reversed" : ""}`}>
         <img src={src} alt={`${name} ${surName}`} className="post-image" />
-        <div>
-          <h2>
-            {name} {surName}
-          </h2>
-          <a href={`mailto:${email}`}>Email: {email}</a>
+        <div className="post-content">
+          <div>
+            <h2>
+              {name} {surName}
+            </h2>
+            <a href={`mailto:${email}`}>Email: {email}</a>
+          </div>
           <p>{text}</p>
         </div>
       </div>
-    </>
+    </div>
   );
 }
--- src/MainPage/MainPage.jsx
+++ src/MainPage/MainPage.jsx
@@ -1,25 +1,27 @@
 import React from "react";
 import Post from "./Components/Post/Post.jsx";
 import Fighters from "../PageData/Data.js";
+import "./MainPage.css";
 
 export default function MainPage() {
   return (
     <div className="blog-container">
       {Fighters.map(({ name, surName, email, text, src }, index) => {
-        const style =
-          index % 2 === 0
-            ? { alignSelf: "flex-start" }
-            : { alignSelf: "flex-end" };
-
+        const isEven = index % 2 === 0;
+        const style = {
+          alignSelf: isEven ? "flex-start" : "flex-end",
+          transform: isEven ? "translateX(-100px)" : "translateX(100px)",
+        };
         return (
           <Post
-            style={style}
             key={index}
             name={name}
             surName={surName}
             email={email}
             text={text}
             src={src}
+            style={style}
+            isReversed={!isEven}
           />
         );
       })}
